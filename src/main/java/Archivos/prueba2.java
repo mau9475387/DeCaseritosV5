1bcf4b2461f36a01a2f2d98a3cf2cc688f65039f
santos colque <82119113+sn-efa@users.noreply.github.com>
2022-06-01 19:49:01 -0400

    PRUEBA de simultaniedad 2

---
+++ src/main/java/Archivos/prueba2.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Archivos;
+
+/**
+ *
+ * @author Colque
+ */
+public class prueba2 {
+    public static void main (String[] args){
+        System.out.println("hola mundo");
+            }
+}
