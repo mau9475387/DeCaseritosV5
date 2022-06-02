48cb76ccb391a5c648a843fdef87b7375c5cd946
Mauricio Almendras Ampuero <106159098+mau9475387@users.noreply.github.com>
2022-06-01 20:32:45 -0400

    Update prueba2.java

--- src/main/java/Archivos/prueba2.java
+++ src/main/java/Archivos/prueba2.java
@@ -11,7 +11,6 @@ package Archivos;
 public class prueba2 {
     public static void main (String[] args){
         System.out.println("hola mundo");
-        System.out.println("qwertyuiop");
-        System.out.println("prueba branch");
+        
             }
 }
